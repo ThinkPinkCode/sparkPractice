b08eb8fff0692f93b7adbb1641076ecf02d7cb5b
Melanie Myers <melanie.myers@365smartshop.com>
2017-04-11 23:03:45 -0400

    added name param

--- src/main/java/com/melaniecodes/todo/main.java
+++ src/main/java/com/melaniecodes/todo/main.java
@@ -1,7 +1,11 @@
 package com.melaniecodes.todo;
 
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
@@ -12,7 +16,13 @@ public class main
     public static void main(String[] args) {
 
         get("/", (req, res) -> new ModelAndView(null, "index.hbs"), new HandlebarsTemplateEngine());
-        post("/signin", (req, res) -> new ModelAndView(null, "signin.hbs"), new HandlebarsTemplateEngine());
+
+
+        post("/signin", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.queryParams("username"));
+            return new ModelAndView(model, "signin.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 
 
